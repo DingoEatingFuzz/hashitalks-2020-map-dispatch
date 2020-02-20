af41a8b280e00e2b44032a417d35a92d9ae0bce1
Michael Lange <dingoeatingfuzz@gmail.com>
2020-02-19 22:37:06 -0800

    Script to dispatch a bunch of jobs

---
+++ infra/all-the-maps.sh
@@ -0,0 +1,25 @@
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Aitoff" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=armadillo" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=azimuthal equal-area" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=azimuthal equidistant" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Baker dinomic" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Bonne" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Bromley" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Butterfly (Waterman)" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Collignon" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Craig retroazimuthal" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Craster parabolic" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Eckert VI" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Equal Earth" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=flat-polar sinusoidal" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=general perspective" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Gingery" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Ginzburg IX" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Guyou square" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Hammer" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Loximuthal" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Mollweide (interrupted hemispheres)" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Natural Earth II" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Peirce quincuncial" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Times" make-map
+nomad job dispatch -meta "DATA_FILE=https://git.io/JvB6U" -meta "PROJECTION=Winkel tripel" make-map
